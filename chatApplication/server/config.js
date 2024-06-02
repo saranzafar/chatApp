1f276cf9dba01a64ffd5d262b9838134f4f999bb
saranzafar <saranzafar736@gmail.com>
2024-06-02 08:04:45 -0700

    changing cors origin from specific to any origin

--- chatApplication/server/config.js
+++ chatApplication/server/config.js
@@ -1,5 +1,6 @@
 // const URL = "http://localhost:5173"
-const URL = "https://chat-app-ten-beige.vercel.app"
+// const URL = "https://chat-app-ten-beige.vercel.app"
+const URL = "*"
 
 export {
     URL,
